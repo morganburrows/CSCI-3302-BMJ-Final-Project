7121175d27d5c6a903def6a855fef34127c7a682
tylermooney86 <tyler.mooney@colorado.edu>
2018-12-13 13:45:48 -0700

    adding face tracking

--- VisionDemo.py
+++ VisionDemo.py
@@ -45,12 +45,13 @@ class UserVision:
         #pil_image.show()
         final_image = pil_image.resize((550,550), Image.ANTIALIAS)
         image_array = np.array(final_image, "uint8")
-        faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=1) # higher scale facter might increase accuracy
+        faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=3) # higher scale facter might increase accuracy
         for (x, y, w, h) in faces:
-            #print(x,y,w,h)
+            print(x,y,w,h)
             print("Detected")
             roi_color = image_array[y:y+h, x:x+w]
             #recid = self.check_5sec()
+            self.adjust_drone_pos(x,y,h)
             #recognizer
             id_, conf = recognizer.predict(roi_color)
             if conf <= 70:  # and conf <= 85:  # 0 is perfect match  200 is max i guess?
@@ -64,30 +65,60 @@ class UserVision:
                     self.faceid = 2
                 else:
                     self.faceid = 3
-                self.perform_action()
+                self.perform_action(x,y,h)
 
-    def perform_action(self):
+    def perform_action(self, x, y, h):
 
         if self.faceid == 0:
-            # self.bebop.flip(back)
+            self.bebop.flip('back')
+            self.bebop.smart_sleep(5)
             print("backflip")
         elif self.faceid == 1:
-            # self.bebop.flip(front)
+            # self.bebop.flip('front')
+            self.bebop.smart_sleep(5)
             print("frontflip")
         elif self.faceid == 2:
-            # self.bebop.turn_degrees(180)
+            # self.bebop.flip('left')
+            self.bebop.smart_sleep(5)
             print('turn around')
         else:
             # ATTACK
+            self.adjust_drone_pos(x,y,h)
             print("attack")
 
         #self.safe_land(10)
+
+    def adjust_drone_pos(self, x, y, h):
+
+        if x < 100:
+            # Turn CCW
+            print("CCW")
+            self.bebop.fly_direct(0,0,-50,0,2)
+            self.bebop.smart_sleep(2)
+
+        elif x > 400:
+            # Turn CW
+            print("CW")
+            self.bebop.fly_direct(0,0,50,0,2)
+            self.bebop.smart_sleep(2)
+    
+        if y < 100: 
+            # increase altitude
+            print("ascend")
+            self.bebop.fly_direct(0,0,0,20,1)
+            self.bebop.smart_sleep(2)
+
+        elif y > 400:
+            # reduce altitude
+            print("descend")
+            self.bebop.fly_direct(0,0,0,-20,1)
+            self.bebop.smart_sleep(2)
 
 
 def demo_user_code_after_vision_opened(bebopVision, args):
     bebop = args[0]
 
-    print("Vision successfully started!")
+    # print("Vision successfully started!")
     #removed the user call to this function (it now happens in open_video())
     #bebopVision.start_video_buffering()
 
@@ -96,16 +127,16 @@ def demo_user_code_after_vision_opened(bebopVision, args):
 
     # skipping actually flying for safety purposes indoors - if you want
     # different pictures, move the bebop around by hand
-    print("Fly me around by hand!")
-    bebop.smart_sleep(5)
+    # print("Fly me around by hand!")
+    # bebop.smart_sleep(5)
 
     if (bebopVision.vision_running):
-        print("Moving the camera using velocity")
-        bebop.pan_tilt_camera_velocity(pan_velocity=0, tilt_velocity=-2, duration=4)
-        bebop.smart_sleep(5)
+        # print("Moving the camera using velocity")
+        # bebop.pan_tilt_camera_velocity(pan_velocity=0, tilt_velocity=-2, duration=4)
+        # bebop.smart_sleep(5)
 
         # land
-        #bebop.safe_land(5)
+        bebop.safe_land(5)
 
         print("Finishing demo and stopping vision")
         bebopVision.close_video()
@@ -128,8 +159,10 @@ if __name__ == "__main__":
         bebopVision = DroneVisionGUI(bebop, is_bebop=True, user_code_to_run=demo_user_code_after_vision_opened,
                                      user_args=(bebop, ))
         userVision = UserVision(bebopVision,bebop)
+
         #take off
-        #bebop.safe_takeoff(10)
+        bebop.safe_takeoff(10)
+        bebop.fly_direct(0,0,0,50,1)
         bebopVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None) #calls save picture continuously
         #bebopVision.set_user_callback_function(perform_action, user_callback_args=(bebop, userVision.faceid ))
 
